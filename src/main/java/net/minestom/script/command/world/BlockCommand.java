23def8623a1fdc422b99ba640c04c803ab1c08bc
TheMode <themode@outlook.fr>
2021-04-13 05:18:22 +0200

    No need for the world uuid inside block set command

--- src/main/java/net/minestom/script/command/world/BlockCommand.java
+++ src/main/java/net/minestom/script/command/world/BlockCommand.java
@@ -1,20 +1,14 @@
 package net.minestom.script.command.world;
 
-import static net.minestom.server.command.builder.arguments.ArgumentType.BlockState;
-import static net.minestom.server.command.builder.arguments.ArgumentType.Literal;
-import static net.minestom.server.command.builder.arguments.ArgumentType.RelativeBlockPosition;
-import static net.minestom.server.command.builder.arguments.ArgumentType.Word;
-
-import java.util.UUID;
-
 import net.kyori.adventure.text.Component;
 import net.minestom.script.command.RichCommand;
 import net.minestom.script.component.RegionComponent;
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.instance.Instance;
+import net.minestom.script.utils.InstanceUtils;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.utils.Position;
+import net.minestom.server.utils.BlockPosition;
 import net.minestom.server.utils.location.RelativeBlockPosition;
+
+import static net.minestom.server.command.builder.arguments.ArgumentType.*;
 
 public class BlockCommand extends RichCommand {
     public BlockCommand() {
@@ -25,12 +19,11 @@ public class BlockCommand extends RichCommand {
         setDefaultExecutor((sender, context) -> sender.sendMessage(Component.text("Usage: /world block <set> worlduuid blockstate")));
         addSyntax((sender, context) -> {
             final Block block = context.get("block");
-            UUID worldUUID = UUID.fromString(context.get("worldId"));
-            final Instance instance = MinecraftServer.getInstanceManager().getInstance(worldUUID);
-
-            RelativeBlockPosition position = context.get("position");
-
-            instance.setBlock(position.from(new Position(0, 0, 0)), block);
-        }, Literal("modify"), Literal("set"), BlockState("block"), Word("worldId"), RelativeBlockPosition("position"));
+            RelativeBlockPosition relativeBlockPosition = context.get("position");
+            BlockPosition blockPosition = relativeBlockPosition.from(sender.isPlayer() ? sender.asPlayer() : null);
+            InstanceUtils.processInstances(sender, instance -> {
+                instance.setBlock(blockPosition, block);
+            });
+        }, Literal("set"), BlockState("block"), RelativeBlockPosition("position"));
     }
 }
