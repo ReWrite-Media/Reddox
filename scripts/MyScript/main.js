d44d92da343b4bc82b06215511a5476d41dc5530
TheMode <themode@outlook.fr>
2021-06-14 21:01:46 +0200

    Update to 1.17

--- scripts/MyScript/main.js
+++ scripts/MyScript/main.js
@@ -1,4 +1,3 @@
-executor.registerCommand("myscript", (sender, context) => {
-    let getPlayer = executor.make("entity query {0}", data => data.entities[0])
-    console.log("random player: "+getPlayer("@p"))
+executor.registerCommand("myscript Player<player>", (sender, context) => {
+    console.log("test: "+context.player)
 })
